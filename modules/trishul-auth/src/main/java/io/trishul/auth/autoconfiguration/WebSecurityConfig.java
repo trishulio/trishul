c7b4a5652ca8473ea067b912d3625253019ff13f
Rishab Manocha <rishab.manocha@outlook.com>
2025-02-02 14:34:16 -0800

    Added WebSecurityConfig

--- modules/trishul-auth/src/main/java/io/trishul/auth/autoconfiguration/WebSecurityConfig.java
+++ modules/trishul-auth/src/main/java/io/trishul/auth/autoconfiguration/WebSecurityConfig.java
@@ -1,27 +1,19 @@
 package io.trishul.auth.autoconfiguration;
 
-// @Configuration
-// public class WebSecurityConfig {
-// @Bean
-// public SecurityFilterChain securityFilterChain(HttpSecurity http) throws
-// Exception {
-// http.authorizeHttpRequests(authorize ->
-// authorize.requestMatchers(
-// "/actuator/**",
-// "/public/**",
-// "/static/**",
-// "/api-docs/**",
-// "/swagger-ui/**",
-// "/swagger-resources/**",
-// "/swagger-ui.html"
-// )
-// .permitAll()
-// .anyRequest()
-// .authenticated()
-// .and()
-// .oauth2ResourceServer()
-// .jwt()
-// );
-// return http.build();
-// }
-// }
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests(authorize -> authorize
+        .requestMatchers("/actuator/**", "/public/**", "/static/**", "/api-docs/**",
+            "/swagger-ui/**", "/swagger-resources/**", "/swagger-ui.html")
+        .permitAll().anyRequest().authenticated()).oauth2ResourceServer(oauth2 -> oauth2.jwt());
+
+    return http.build();
+  }
+}
