9e20e87ba0bb8a49d4ec58e6c8a7e7130ed7b2c1
Rishab Manocha <rishab.manocha@outlook.com>
2026-06-13 18:15:34 -0700

    Fix build issues

--- modules/trishul-app/src/test/java/io/trishul/app/Application.java
+++ modules/trishul-app/src/test/java/io/trishul/app/Application.java
@@ -1,19 +1,15 @@
 package io.trishul.app;
 
 import io.trishul.tenant.persistence.config.PackageScanConfig;
-import java.util.HashMap;
-import java.util.Map;
-import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication(scanBasePackages = { "io.trishul" })
-@EnableJpaRepositories(basePackages = { "io.trishul" })
+@SpringBootApplication(scanBasePackages = {"io.trishul"})
+@EnableJpaRepositories(basePackages = {"io.trishul"})
 @EnableTransactionManagement
 public class Application {
   public static void main(String[] args) {
